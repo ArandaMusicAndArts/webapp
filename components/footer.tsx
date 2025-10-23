21cf02dd1bf795a47cdeacdea95d92f7b65552d3
Ingo Wolf <me@ingowolf.au>
2025-10-23 16:41:04 +1100

    add footer

---
+++ components/footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="border-t border-dashed">
+      <div className="container mx-auto p-4">
+        <p>Copyright © 2023</p>
+      </div>
+    </footer>
+  );
+}
