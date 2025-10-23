4e97e1e34f72158b5b670f29d054a4283e630313
Ingo Wolf <me@ingowolf.au>
2025-10-23 16:43:06 +1100

    update footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,8 +1,23 @@
+import Link from "next/link";
+
 export default function Footer() {
   return (
     <footer className="border-t border-dashed">
       <div className="container mx-auto p-4">
-        <p>Copyright © 2023</p>
+        <p>
+          Website is built by{" "}
+          <Link href="https://ingo.au" className="underline" target="_blank">
+            Ingo
+          </Link>{" "}
+          and is{" "}
+          <Link
+            href="https://github.com/ArandaMusicAndArtsProgram/webapp"
+            className="underline"
+            target="_blank"
+          >
+            open source
+          </Link>
+        </p>
       </div>
     </footer>
   );
