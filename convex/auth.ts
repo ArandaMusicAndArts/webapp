18b2b8ca40c47f444ddf1f5a3d3b66bd6ecf6994
Ingo Wolf <me@ingowolf.au>
2025-10-23 14:54:16 +1100

    disable email password auth

--- convex/auth.ts
+++ convex/auth.ts
@@ -27,8 +27,7 @@ export const createAuth = (
     database: authComponent.adapter(ctx),
     // Configure simple, non-verified email/password to get started
     emailAndPassword: {
-      enabled: true,
-      requireEmailVerification: false,
+      enabled: false,
     },
     socialProviders: {
       google: {
