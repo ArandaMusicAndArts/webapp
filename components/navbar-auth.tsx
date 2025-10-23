6ef21fef50ce080f82e988d0ed7d5ef5f0f09082
Ingo Wolf <me@ingowolf.au>
2025-10-23 15:42:45 +1100

    add skeleton for loading

--- components/navbar-auth.tsx
+++ components/navbar-auth.tsx
@@ -1,13 +1,18 @@
 "use client";
 
-import { UserButton } from "@daveyplate/better-auth-ui";
+import { AuthLoading, UserButton } from "@daveyplate/better-auth-ui";
 import { Authenticated, Unauthenticated } from "convex/react";
 import { Button } from "./ui/button";
 import Link from "next/link";
+import { Skeleton } from "./ui/skeleton";
 
 export function NavbarAuth() {
   return (
     <div className="flex gap-2">
+      <AuthLoading>
+        <Skeleton className="h-9 w-20"></Skeleton>
+        <Skeleton className="h-9 w-16"></Skeleton>
+      </AuthLoading>
       <Authenticated>
         <UserButton size="icon" />
       </Authenticated>
