e35c5a129cc3eae2474b455c426ec2db22d8e54f
Ingo Wolf <me@ingowolf.au>
2025-10-23 15:29:37 +1100

    add navbar auth component

---
+++ components/navbar-auth.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { UserButton } from "@daveyplate/better-auth-ui";
+import { Authenticated, Unauthenticated } from "convex/react";
+import { Button } from "./ui/button";
+import Link from "next/link";
+
+export default function NavbarAuth() {
+  return (
+    <div className="flex gap-2">
+      <Authenticated>
+        <UserButton size="icon" />
+      </Authenticated>
+      <Unauthenticated>
+        <Button variant="outline" asChild>
+          <Link href="/auth/sign-in">Login</Link>
+        </Button>
+        <Button asChild>
+          <Link href="/auth/sign-in">Enrol</Link>
+        </Button>
+      </Unauthenticated>
+    </div>
+  );
+}
